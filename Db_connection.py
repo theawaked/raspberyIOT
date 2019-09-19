519cfc2b4c347e70747d57dedd6f31e33f1fbe09
gerben bunt <g.bunt@st.hanze.nl>
2019-09-19 12:48:05 +0200

    db connection file -> no date and time included

--- Db_connection.py
+++ Db_connection.py
@@ -1,7 +1,20 @@
-#!/usr/bin/env python
+import sqlite3
 
-import MySQLdb
+sqlite_file = '/home/Desktop/program/sensordata.'
+
+conn = sqlite3.connect(sqlite_file)
+c = conn.cursor()
+
+table_name = "sensorreadings"
+temperature_column = "temperature"
+humidity_column = "humidity"
+pressure_column = "pressure"
+
+try:
+    c.execute("INSERT INTO {tn} ({cn1}, {cn2}, {cn3}) VALUES (123456, 123456, 123456)".\
+        format(tn=table_name, cn1=temperature_column, cn2=humidity_column, cn3=pressure_column))
+except sqlite3.IntegrityError:
+    print('ERROR: integerityError')
 
 
-db = MySQLdb.connect("localhost", "administrator", "password", "temps")
-curs=db.cursor()
+conn.close()
