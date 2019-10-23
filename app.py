ac93cb9ef7bbd7b138aa907050610e2a56ed7c9b
gerben bunt <g.bunt@st.hanze.nl>
2019-10-23 02:28:17 +0200

    dwerwer

--- app.py
+++ app.py
@@ -107,13 +107,13 @@ def send_confirmation_callback(message, result, user_context):
     print ( "    message_id: %s" % message.message_id )
     print ( "    correlation_id: %s" % message.correlation_id )
     key_value_pair = map_properties.get_internals()
-    if str(result) == 'MESSAGE_TIMEOUT':
-        for i in key_value_pair:	   
-            print (key_value_pair[i])
-           
-        print (key_value_pair[1])   
-        print("message timedout saving variables locally")
-        database.insert_dbvalues(databaseconnection,key_value_pair[1],key_value_pair[2],key_value_pair[3])
+    #if str(result) == 'MESSAGE_TIMEOUT':
+    for i in key_value_pair:	   
+        print (key_value_pair[i])
+        
+    print (key_value_pair[1])   
+    print("message timedout saving variables locally")
+    database.insert_dbvalues(databaseconnection,key_value_pair[1],key_value_pair[2],key_value_pair[3])
 
     print ( "    Properties: %s" % key_value_pair )
     SEND_CALLBACKS += 1
